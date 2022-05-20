c5a586cb2ddbe973728cab91944ffb4f53ee2770
muhammadfarouk2394 <muhammadfarouk2394@gmail.com>
2022-05-20 22:32:24 +0200

    Finished the analysis componenet

---
+++ analysis.py
@@ -0,0 +1,16 @@
+from arangoclient import client
+
+def get_taxi_analysis():
+    
+
+    db = client.db("free-now", username="root", password="1234")
+
+    if not db.has_collection("article"):
+        return {}
+    cursor = db.aql.execute('''for a in article
+                               LET taxi_count = LENGTH(REGEX_SPLIT(a.content, " taxi ", true))-1
+                               LET link = a.url
+                               return { link , taxi_count}
+    ''')
+    result = [document for document in cursor]
+    return result    
